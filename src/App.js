f0e3c2a9d20f3fcb30a46e9d1b4772dc94471c72
Jason Kaskel <jason.kaskel@gmail.com>
2020-11-10 14:50:35 -0500

    sofl detail plot

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ const avg = (ary, source) =>
 const App = () => {
   const allByDate = {};
   const soflByDate = {};
+  const browardByDate = {};
+  const dadeByDate = {};
+  const palmBeachByDate = {};
   Object.keys(casesByCountyAndDate).forEach((county) => {
     Object.keys(casesByCountyAndDate[county]).forEach((date) => {
       const normalizedDate = moment(Number.parseInt(date))
@@ -26,9 +29,25 @@ const App = () => {
         allByDate[normalizedDate] = { total: 0, weekly_avg: 0 };
       if (!soflByDate[normalizedDate])
         soflByDate[normalizedDate] = { total: 0, weekly_avg: 0 };
+      if (!browardByDate[normalizedDate])
+        browardByDate[normalizedDate] = { total: 0, weekly_avg: 0 };
+      if (!dadeByDate[normalizedDate])
+        dadeByDate[normalizedDate] = { total: 0, weekly_avg: 0 };
+      if (!palmBeachByDate[normalizedDate])
+        palmBeachByDate[normalizedDate] = { total: 0, weekly_avg: 0 };
       allByDate[normalizedDate].total += casesByCountyAndDate[county][date];
       if (SOFL_COUNTIES.includes(county)) {
         soflByDate[normalizedDate].total += casesByCountyAndDate[county][date];
+        if (county === "Broward") {
+          browardByDate[normalizedDate].total +=
+            casesByCountyAndDate[county][date];
+        } else if (county === "Dade") {
+          dadeByDate[normalizedDate].total +=
+            casesByCountyAndDate[county][date];
+        } else if (county === "Palm Beach") {
+          palmBeachByDate[normalizedDate].total +=
+            casesByCountyAndDate[county][date];
+        }
       }
     });
   });
@@ -42,6 +61,18 @@ const App = () => {
     soflByDate[normalizedDate].weekly_avg = avg(
       allX.slice(from, i + 1),
       soflByDate
+    );
+    browardByDate[normalizedDate].weekly_avg = avg(
+      allX.slice(from, i + 1),
+      browardByDate
+    );
+    dadeByDate[normalizedDate].weekly_avg = avg(
+      allX.slice(from, i + 1),
+      dadeByDate
+    );
+    palmBeachByDate[normalizedDate].weekly_avg = avg(
+      allX.slice(from, i + 1),
+      palmBeachByDate
     );
   });
 
@@ -85,6 +116,70 @@ const App = () => {
         layout={{
           autosize: true,
           title: "SoFl Covid Cases",
+          yaxis: {
+            rangemode: "tozero",
+          },
+        }}
+        useResizeHandler
+        style={{ width: "100%", height: "100%" }}
+      />
+      <br />
+      <Plot
+        data={[
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) => Math.round(browardByDate[stamp].weekly_avg)),
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "blue" },
+            name: "Broward",
+          },
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) => Math.round(dadeByDate[stamp].weekly_avg)),
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "red" },
+            name: "Dade",
+          },
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) =>
+              Math.round(palmBeachByDate[stamp].weekly_avg)
+            ),
+            type: "scatter",
+            mode: "lines+markers",
+            marker: { color: "green" },
+            name: "Palm Beach",
+          },
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) => Math.round(browardByDate[stamp].total)),
+            type: "bar",
+            mode: "lines+markers",
+            marker: { color: "#61699e" },
+            name: "Broward (total)",
+          },
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) => Math.round(dadeByDate[stamp].total)),
+            type: "bar",
+            mode: "lines+markers",
+            marker: { color: "#fd9e9e" },
+            name: "Dade (total)",
+          },
+          {
+            x: allX.map((stamp) => formatDate(stamp)),
+            y: allX.map((stamp) => Math.round(palmBeachByDate[stamp].total)),
+            type: "bar",
+            mode: "lines+markers",
+            marker: { color: "#8df00f" },
+            name: "Palm Beach (total)",
+          },
+        ]}
+        layout={{
+          autosize: true,
+          title: "SoFl Covid Case Details",
           yaxis: {
             rangemode: "tozero",
           },
